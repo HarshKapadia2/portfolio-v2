b7e795ea0178b97fbcc28b09a185d60878019069
Harsh Kapadia <harshgkapadia@gmail.com>
2021-11-18 15:24:46 +0530

    :truck: update: Add burger menu to Navbar

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+const ul = document.querySelector(".navbar-ul");
+const burger_menu = document.querySelector(".burger-menu");
+
+burger_menu.addEventListener("click", (event) => {
+	ul.classList.toggle("hide-burger-menu");
+	event.stopPropagation();
+});
+
+window.addEventListener("click", () => {
+	ul.classList.add("hide-burger-menu");
+});
