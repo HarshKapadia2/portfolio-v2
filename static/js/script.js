3381a8b0756534d7b10c54d0e86d75cfa129b745
Auto deploy GitHub Pages <cd-deploy-gh-pages@bot>
2021-11-19 20:49:59 +0000

    Deploy to pages: 2021-11-19 20:49:59

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,22 @@
 const ul = document.querySelector(".navbar-ul");
-const burger_menu = document.querySelector(".burger-menu");
+const burgerMenu = document.querySelector(".burger-menu");
+const header = document.querySelector("header");
+const headerTitle = document.querySelector("#header-title");
+const borderDisplayIndicator = document.querySelector(
+	"#header-border-indicator"
+);
+const main = document.querySelector("main");
 
-burger_menu.addEventListener("click", (event) => {
+// Header title
+const observer1 = new IntersectionObserver(displayHeaderTitle);
+observer1.observe(main);
+
+// Header border
+const observer2 = new IntersectionObserver(displayHeaderBorder);
+observer2.observe(borderDisplayIndicator);
+
+// For burger menu
+burgerMenu.addEventListener("click", (event) => {
 	ul.classList.toggle("hide-burger-menu");
 	event.stopPropagation();
 });
@@ -9,3 +24,19 @@ burger_menu.addEventListener("click", (event) => {
 window.addEventListener("click", () => {
 	ul.classList.add("hide-burger-menu");
 });
+
+// Utility functions
+function displayHeaderTitle(entries, observer) {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting)
+			headerTitle.classList.add("hidden-visibility");
+		else headerTitle.classList.remove("hidden-visibility");
+	});
+}
+
+function displayHeaderBorder(entries, observer) {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) header.classList.remove("header-border");
+		else header.classList.add("header-border");
+	});
+}
